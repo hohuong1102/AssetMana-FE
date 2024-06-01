c3f046c78bf7340fecdc2115471c64908c1fe92c
FRANKHO <hohuong1102@gmail.com>
2024-06-01 10:56:35 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [departments, setDepartments] = useState([]);
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/departments')
+      .then(response => {
+        setDepartments(response.data);
+      });
+  }, []);
+
+  const handleAddDepartment = () => {
+    axios.post('http://localhost:8000/departments', { name })
+      .then(response => {
+        setDepartments([...departments, response.data]);
+        setName('');
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Departments</h1>
+      <ul>
+        {departments.map(department => (
+          <li key={department.id}>{department.name}</li>
+        ))}
+      </ul>
+      <input 
+        type="text" 
+        value={name} 
+        onChange={e => setName(e.target.value)} 
+        placeholder="New Department"
+      />
+      <button onClick={handleAddDepartment}>Add</button>
     </div>
   );
 }
